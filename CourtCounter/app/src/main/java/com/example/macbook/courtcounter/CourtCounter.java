f47ec73ce2b9d774c182320fcc8c72a576850ddf
Surjit4048 <Surjitgit1>
2016-11-11 13:43:26 -0600

    made it a bit pretty. completed problem set 2

--- CourtCounter/app/src/main/java/com/example/macbook/courtcounter/CourtCounter.java
+++ CourtCounter/app/src/main/java/com/example/macbook/courtcounter/CourtCounter.java
@@ -1,13 +1,66 @@
 package com.example.macbook.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class CourtCounter extends AppCompatActivity {
+    int teamAScore = 0;
+    int teamBScore = 0;
+    private TextView teamAScoreView;
+    private TextView teamBScoreView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_court_counter);
+        teamAScoreView = (TextView) findViewById(R.id.team_a_score);
+        teamBScoreView = (TextView) findViewById(R.id.team_b_score);
+    }
+
+    public void displayForTeamA(int score) {
+        teamAScoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeToTeamA(View view) {
+        teamAScore += 3;
+        displayForTeamA(teamAScore);
+    }
+
+    public void addTwoToTeamA(View view) {
+        teamAScore += 2;
+        displayForTeamA(teamAScore);
+    }
+
+    public void addFreeThrowToTeamA(View view) {
+        teamAScore += 1;
+        displayForTeamA(teamAScore);
+    }
+
+    public void addThreeToTeamB(View view) {
+        teamBScore += 3;
+        displayForTeamB(teamBScore);
+    }
+
+    public void addTwoToTeamB(View view) {
+        teamBScore += 2;
+        displayForTeamB(teamBScore);
+    }
+
+    public void addFreeThrowToTeamB(View view) {
+        teamBScore += 1;
+        displayForTeamB(teamBScore);
+    }
+
+    private void displayForTeamB(int teamBScore) {
+        teamBScoreView.setText(String.valueOf(teamBScore));
+    }
+
+    public void resetScore(View view) {
+        teamAScore = 0;
+        teamBScore = 0;
+        displayForTeamA(teamAScore);
+        displayForTeamB(teamBScore);
     }
 }
